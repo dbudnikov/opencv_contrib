d5fd7f4232b15c295a98cc8c83ed1e0edab4e5f0
Dmitriy Budnikov <d.budnikov@yadro.com>
2023-05-04 11:23:18 +0300

    log transform profiling (Mat version)

--- modules/intensity_transform/src/intensity_transform.cpp
+++ modules/intensity_transform/src/intensity_transform.cpp
@@ -7,9 +7,16 @@
 using namespace cv;
 using namespace std;
 
+#include <iostream>
+#define LOG_TRANSFORM_PERF_PROFILE
+
 namespace cv {
 namespace intensity_transform {
 
+
+
+
+#if 0
 void logTransform(const Mat input, Mat& output)
 {
     double maxVal;
@@ -21,6 +28,57 @@ void logTransform(const Mat input, Mat& output)
     cv::log(add_one_64f, log_64f);
     log_64f.convertTo(output, CV_8UC3, c, 0.0f);
 }
+#else
+void logTransform(const Mat input, UMat& output)
+{
+#ifdef LOG_TRANSFORM_PERF_PROFILE
+    cv::TickMeter tm_all, tm_minmax, tm_log, tm_cvt1, tm_cvt2;
+    tm_all.start();
+#endif
+
+    double maxVal;
+#ifdef LOG_TRANSFORM_PERF_PROFILE
+    tm_minmax.start();
+#endif
+    minMaxLoc(input, NULL, &maxVal, NULL, NULL);
+#ifdef LOG_TRANSFORM_PERF_PROFILE   
+    tm_minmax.stop();
+#endif
+
+    const double c = 255 / log(1 + maxVal);
+    Mat add_one_64f(cv::USAGE_ALLOCATE_DEVICE_MEMORY);
+#ifdef LOG_TRANSFORM_PERF_PROFILE
+    tm_cvt1.start();
+#endif
+    input.convertTo(add_one_64f, CV_64F, 1, 1.0f);
+#ifdef LOG_TRANSFORM_PERF_PROFILE
+    tm_cvt1.stop();
+#endif
+    Mat log_64f(cv::USAGE_ALLOCATE_DEVICE_MEMORY);
+#ifdef LOG_TRANSFORM_PERF_PROFILE
+    tm_log.start();
+#endif
+    cv::log(add_one_64f, log_64f);
+#ifdef LOG_TRANSFORM_PERF_PROFILE
+    tm_log.stop();
+#endif
+#ifdef LOG_TRANSFORM_PERF_PROFILE
+    tm_cvt2.start();
+#endif
+    log_64f.convertTo(output, CV_8UC3, c, 0.0f);
+#ifdef LOG_TRANSFORM_PERF_PROFILE
+    tm_cvt2.stop();
+#endif
+#ifdef LOG_TRANSFORM_PERF_PROFILE   
+    tm_all.stop();
+    std::cout << "logTransform ALL " << tm_all.getTimeSec() << " sec)" << std::endl;
+    std::cout << "logTransform minMaxLoc " << tm_minmax.getTimeSec() << " sec)" << std::endl;
+    std::cout << "logTransform input.convertTo add_one_64f " << tm_cvt1.getTimeSec() << " sec)" << std::endl;
+    std::cout << "logTransform cv::log " << tm_log.getTimeSec() << " sec)" << std::endl;
+    std::cout << "logTransform log_64f.convertTo output " << tm_cvt2.getTimeSec() << " sec)" << std::endl;
+#endif
+}
+#endif
 
 void gammaCorrection(const Mat input, Mat& output, const float gamma)
 {
